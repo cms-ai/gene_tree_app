732824d7b5cb2c733c09e91493b6000d1e2c43d4
aitc.os <tcai2000ely@gmail.com>
2025-02-22 20:24:35 +0700

    * update create, update clan member screen

--- gene_tree_app/lib/modules/main/container/clan_member/create_clan_member/models/create_clan_member_argument.dart
+++ gene_tree_app/lib/modules/main/container/clan_member/create_clan_member/models/create_clan_member_argument.dart
@@ -1,5 +1,12 @@
 part of '../create_clan_member_screen.dart';
 
+enum CreateClanMemberScreenType {
+  create,
+  update,
+}
+
 class CreateClanMemberArgument {
-  const CreateClanMemberArgument();
+  const CreateClanMemberArgument({
+    CreateClanMemberScreenType type = CreateClanMemberScreenType.create,
+  });
 }
